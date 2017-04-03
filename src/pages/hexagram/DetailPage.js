cc1e298955a4ac91162a4c0447565dabb45d4065
rainbow corals <barrabin.fc@gmail.com>
2017-04-02 22:05:19 -0300

    Better formatting of quotes for detail page

--- src/pages/hexagram/DetailPage.js
+++ src/pages/hexagram/DetailPage.js
@@ -12,6 +12,10 @@ import { connect } from 'react-redux'
 
 
 let DetailPage = React.createClass({
+  formatQuote(text) {
+    return text.replace(/\t/g,'')
+  },
+
   render() {
     let hexNumber = _.toNumber( this.props.params.number );
 
@@ -23,42 +27,42 @@ let DetailPage = React.createClass({
 
     let lines    = _.chain( hex.interpretation.lines ).map( (line,i) => (
       <div className="line" key={i}>
-        <blockquote>{line.poem}</blockquote>
-        <p>{line.expl}</p>
+          <q className="subQuote">{line.poem}</q>
+          <p>{line.expl}</p>
       </div>
-          )).value()
+    )).value()
 
     let tarot_class = classNames({[`icon-Tao_${hex.number}`]: true});
     return (
       <div className="detailspage-container">
-        <HexagramInfoCard hexagram={hex} trigrams />
+          <HexagramInfoCard hexagram={hex} trigrams />
 
-        <div className="interpretation">
+          <div className="interpretation">
 
-          <div className="highlight">
-            <div className="tarot">
-              <div className={tarot_class} />
-            </div>
-            <div className="oracle">
-              <blockquote>{hex.interpretation.oracle}</blockquote>
-            </div>
+              <div className="highlight">
+                  <div className="tarot">
+                      <div className={tarot_class} />
+                  </div>
+                  <div className="oracle">
+                      <q>{this.formatQuote(hex.interpretation.oracle)}</q>
+                  </div>
+              </div>
+              <p>{hex.interpretation.resume}</p>
+
+              <h3>The Image</h3>
+              <Divider />
+              <q className="subQuote">{this.formatQuote(hex.interpretation.image.oracle)}</q>
+              <p>{hex.interpretation.image.image}</p>
+
+              <h3>The Judgement</h3>
+              <Divider />
+              <q className="subQuote">{this.formatQuote(hex.interpretation.oracle)}</q>
+              <p>{hex.interpretation.judgment}</p>
+
+              <h3>The Lines</h3>
+              <Divider />
+              {lines}
           </div>
-          <p>{hex.interpretation.resume}</p>
-
-          <h3>The Image</h3>
-          <Divider />
-          <pre>{hex.interpretation.image.oracle}</pre>
-          <p>{hex.interpretation.image.image}</p>
-
-          <h3>The Judgement</h3>
-          <Divider />
-          <pre>{hex.interpretation.oracle}</pre>
-          <p>{hex.interpretation.judgment}</p>
-
-          <h3>The Lines</h3>
-          <Divider />
-          {lines}
-        </div>
       </div>
     );
   },
