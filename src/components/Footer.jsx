b428f313d5940128c49e8e170de9cf2ecae257aa
rainbow corals <barrabin.fc@gmail.com>
2017-12-10 14:20:49 -0200

    Testing eslint commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
-import {Component} from 'preact';
+import { Component } from "preact";
 
-import {connect} from 'preact-redux'
+import { connect } from "preact-redux";
 
-import { NavLink } from 'react-router-dom';
-import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
+import { NavLink } from "react-router-dom";
+import CSSTransitionGroup from "react-transition-group/CSSTransitionGroup";
 
-import { withRouter } from 'react-router'
+import { withRouter } from "react-router";
 
-import YinYangSvg from "../public/icons/yin_yang.svg"
+import YinYangSvg from "../public/icons/yin_yang.svg";
 
 function Footer() {
   return (
@@ -17,20 +17,21 @@ function Footer() {
                           transitionAppearTimeout={1000}
                           transitionEnterTimeout={1000}
                           transitionLeaveTimeout={1000}> */
+
     <div className="footer icon-bar">
-        <NavLink to="/" activeClassName="active">
-          <i className="box-block center-block icon my-icon yinyang" />
-          <span className="label">Play</span>
-        </NavLink>
-        <NavLink to="/about" activeClassName="active">
-          <i className="box-block center-block icon my-icon link" />
-          <span className="label">Settings</span>
-        </NavLink>
-      </div>
+      <NavLink to="/" activeClassName="active">
+        <i className="box-block center-block icon my-icon yinyang" />
+        <span className="label">Play</span>
+      </NavLink>
+      <NavLink to="/about" activeClassName="active">
+        <i className="box-block center-block icon my-icon link" />
+        <span className="label">Settings</span>
+      </NavLink>
+    </div>
     /*
     </CSSTransitionGroup>
     */
   );
 }
 
-export default connect( state => ({}) )(Footer)
+export default connect(state => ({}))(Footer);
