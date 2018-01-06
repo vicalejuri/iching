5ada7dfcc89a8fa6f720a26dfeb96d2ef8443506
Vitor <barrabin.fc@gmail.com>
2018-01-06 16:11:59 -0200

    Development on windows machine

--- src/constants/utils.js
+++ src/constants/utils.js
@@ -6,3 +6,12 @@ export function getAsset( resource_path ) {
 export function CSSVar( varName , rootCtx = document.body) {
   return rootCtx.style.getProperty(varName);
 }
+
+/**
+ * Generate a crypto UUID RFC4122 compliant
+ * https://30secondsofcode.org/#uuidgeneratorbrowser
+ */
+const UUIDGenerator = () =>
+  ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+    (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
+  );
