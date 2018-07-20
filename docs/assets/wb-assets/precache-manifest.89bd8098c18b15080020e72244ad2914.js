390450918c73e5ab50c962597dc8bc50f49d9715
rcoral <barrabin.fc@gmail.com>
2018-07-20 17:03:44 -0300

    New build with fixed manifest and PUBLIC_PATH

---
+++ docs/assets/wb-assets/precache-manifest.89bd8098c18b15080020e72244ad2914.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f0a6526cd88f164760c5738148c8657",
+    "url": "/iching//assets/fonts/eb-garamond-latin-500italic.woff"
+  },
+  {
+    "revision": "a6187c038f8436ab219fd1c1a7419aa2",
+    "url": "/iching//assets/fonts/eb-garamond-latin-800italic.woff2"
+  },
+  {
+    "revision": "a7754eef33a55f8a3268",
+    "url": "/iching//assets/vendor.js"
+  },
+  {
+    "revision": "6ca1966fe594dcffb7c4",
+    "url": "/iching//assets/main.css"
+  },
+  {
+    "revision": "ccde8fe42f7b396ff4e9af40cefb4773",
+    "url": "/iching//assets/fonts/yin_yang.svg"
+  },
+  {
+    "revision": "e9ade79c17ec437980d0393b9b72da5b",
+    "url": "/iching//assets/fonts/yin_yang_filled.svg"
+  },
+  {
+    "revision": "57d17086b493702cd3eb086a77c8f58f",
+    "url": "/iching//assets/fonts/eb-garamond-latin-600.woff2"
+  },
+  {
+    "revision": "5d8985035817216d4b79917740a9a728",
+    "url": "/iching//assets/fonts/Cormorant-Italic.woff2"
+  },
+  {
+    "revision": "7678707a2677cab3caf55be262d473e1",
+    "url": "/iching//assets/fonts/eb-garamond-latin-400.woff2"
+  },
+  {
+    "revision": "f2896b48bc85cf58ccb25b44355744b1",
+    "url": "/iching//assets/fonts/eb-garamond-latin-400italic.woff"
+  },
+  {
+    "revision": "b9d2dc39909327549b3e72ea02796862",
+    "url": "/iching//assets/fonts/eb-garamond-latin-400italic.woff2"
+  },
+  {
+    "revision": "491153d8f5f5c823d1aa437bd85fcfc8",
+    "url": "/iching//assets/fonts/eb-garamond-latin-500.woff"
+  },
+  {
+    "revision": "d3fd4b0cfd4624d0990e0469c5eec051",
+    "url": "/iching//assets/fonts/eb-garamond-latin-500.woff2"
+  },
+  {
+    "revision": "be961f6737fbcad4c50e01c08fb1c102",
+    "url": "/iching//assets/fonts/eb-garamond-latin-800italic.woff"
+  },
+  {
+    "revision": "8cdafd2e5a6a33e5c87da493019859e1",
+    "url": "/iching//assets/fonts/eb-garamond-latin-500italic.woff2"
+  },
+  {
+    "revision": "925bcbbc9f96204641d06df7b48819b1",
+    "url": "/iching//assets/fonts/eb-garamond-latin-600.woff"
+  },
+  {
+    "revision": "ee1e456d475733f7e8f45ae77eeed952",
+    "url": "/iching//assets/fonts/eb-garamond-latin-400.woff"
+  },
+  {
+    "revision": "70fed04547ba09465c8c5c848d5c45d5",
+    "url": "/iching//assets/fonts/eb-garamond-latin-600italic.woff"
+  },
+  {
+    "revision": "0e91a57dd8cdf2900eadc0dd86296a91",
+    "url": "/iching//assets/fonts/eb-garamond-latin-600italic.woff2"
+  },
+  {
+    "revision": "d7f67fb6ed615a03ae74e2ae51e22c02",
+    "url": "/iching//assets/fonts/eb-garamond-latin-700.woff"
+  },
+  {
+    "revision": "ced5f3f1dc86a54e99ac8833baa3ca0c",
+    "url": "/iching//assets/fonts/eb-garamond-latin-700.woff2"
+  },
+  {
+    "revision": "71e2bf2a4d659e386bb21195d349793e",
+    "url": "/iching//assets/fonts/eb-garamond-latin-700italic.woff"
+  },
+  {
+    "revision": "c08e89ceac648b57c2780436eb84b69e",
+    "url": "/iching//assets/fonts/eb-garamond-latin-700italic.woff2"
+  },
+  {
+    "revision": "739eafb0fd3a64e09518b93953b6b5a0",
+    "url": "/iching//assets/fonts/eb-garamond-latin-800.woff"
+  },
+  {
+    "revision": "151b1ad7103446b5e811c8efdf99ed38",
+    "url": "/iching//assets/fonts/eb-garamond-latin-800.woff2"
+  },
+  {
+    "revision": "2de5d3cae50367b979c8",
+    "url": "/iching//assets/book.js"
+  },
+  {
+    "revision": "6ca1966fe594dcffb7c4",
+    "url": "/iching//assets/app.js"
+  }
+];
